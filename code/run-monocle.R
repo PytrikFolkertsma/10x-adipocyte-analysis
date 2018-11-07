50896a96e8709304cd9e3753bf093512f1742a1f
PytrikFolkertsma <pytrik_27@live.nl>
2018-11-07 14:22:42 +0100

    monocle analysis notebook

--- code/run-monocle.R
+++ code/run-monocle.R
@@ -15,7 +15,7 @@ opt <- parse_args(optionparser)
 
 if (is.null(opt$file) || is.null(opt$outdir)){
   print_help(optionparser)
-  quit()  
+  quit()
 }
 
 if (!(opt$regressout == 'none')){
@@ -43,20 +43,23 @@ output_prefix <- unlist(strsplit(opt$file, '/'))[length(unlist(strsplit(opt$file
 output_prefix <- paste(output_prefix, 'monocle', sep='-')
 
 run_monocle_workflow <- function(data){
-  
+
   print('Converting Seurat object to CDS...')
   cds <- importCDS(data)
+
+  #if column sample_name exists, replace with samplename, otherwise Monocle will complain about duplicate column names.
+  colnames(pData(cds)) <- replace(colnames(pData(cds)), colnames(pData(cds)) == 'sample_name', 'samplename')
 
   ###TRAJECTORY STEP 1: Choose genes that define a cell's process
   # There are a few ways to do this:
   # - For time series data: Do a differential gene test between cells collected at
   #   the beginning of the experiment and those collected at the end. Did this the
   #   first time, but only the general adipogenesis genes are found then.
-  # - For non time series data: 
-  #     - (Used here) Use differentially exressed genes between clusters for ordering. 
-  #       Monocle's dpFeature does this, but this takes an extremly long time so I 
-  #       obtained the DE genes for cluster res.1.5 with Seurat. 
-  #     - Select genes with high dispersion across cells. 
+  # - For non time series data:
+  #     - (Used here) Use differentially exressed genes between clusters for ordering.
+  #       Monocle's dpFeature does this, but this takes an extremly long time so I
+  #       obtained the DE genes for cluster res.1.5 with Seurat.
+  #     - Select genes with high dispersion across cells.
   #     - Use known marker genes.
 
   if (is.null(opt$genelist)){
@@ -72,7 +75,7 @@ run_monocle_workflow <- function(data){
   }
 
   print(paste('Nr of genes:', length(ordering_genes)))
-  
+
   print('Seting ordering filter...')
   cds <- setOrderingFilter(cds, ordering_genes)
 
@@ -91,12 +94,12 @@ run_monocle_workflow <- function(data){
   }
 
   ###TRAJECTORY STEP 3: Order cells along the trajectory
-  
+
   print('Ordering cells along trajectory...')
   cds <- orderCells(cds)
-  
+
   ###SAVE CDS
-  
+
   if (opt$regressout == 'none' && is.null(opt$downsample_col)){
     output_name <- output_prefix
   } else if (opt$regressout == 'none' && !is.null(opt$downsample_col)){
@@ -106,7 +109,7 @@ run_monocle_workflow <- function(data){
   } else if (opt$regressout != 'none' && !is.null(opt$downsample_col)){
     output_name <- paste(output_prefix, opt$regressout, opt$downsample_seed, sep='-')
   }
-  
+
   print(paste('Saving CDS as', output_name, 'in', opt$outdir))
   saveRDS(cds, paste(opt$outdir, output_name, sep=''))
 
